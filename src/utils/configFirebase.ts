e413efd0dc9409bf7a5060393dc035009f4ef1f7
Apurbalal Singha <apurbalal@mercari.com>
2024-05-20 01:22:31 +0900

    remove analytics

--- src/utils/configFirebase.ts
+++ src/utils/configFirebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFunctions } from "firebase/functions";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -22,4 +21,3 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const functions = getFunctions(app);
-const analytics = getAnalytics(app);
